0cbfc6fb6136b79a94c3eb9a2d5598149d7afa1a
lance <atrpwq@proton.me>
2024-11-10 21:37:33 +0300

    made it a little better lol

--- main.c
+++ main.c
@@ -2,24 +2,33 @@
 #include<string.h>
 #include<stdlib.h>
 
-int editfile(char filename[8192],char text[16384]){
-	FILE *file;
-	file = fopen(filename,"a");
-	fprintf(file,text);
-	fclose(file);
+#define __CLEAR_SCREEN system("clear")
+
+void editfile(char filename[8192],char text[16384]){
+        FILE *file;
+        file = fopen(filename,"a");
+        fprintf(file,"%s",text);
+        fclose(file);
 }
 
-void main(){
-	char text[16384];
-	char f[8192];
-	printf("file: ");fgets(f,8192,stdin);
-	f[strlen(f)-1]='\0';
-	system("clear");
-	while(1){
-		printf("~ ");fgets(text,16384,stdin);
-		if(strcmp(text,"EXITWTE\n")==0){
-			exit(1);
-		}
-		editfile(f,text);
-	}
+int main(){
+        char text[16384];
+        char f[8192];
+        printf("file: ");
+        fgets(f,8192,stdin);
+        f[strlen(f)-1]='\0';
+        __CLEAR_SCREEN;
+        int i=1;
+        while(1){
+                printf("%d ",i);
+                fgets(text,16384,stdin);
+                if(strcmp(text,"!EXITWTE\n")==0){
+                        __CLEAR_SCREEN;
+                        return 0;
+                }
+                editfile(f,text);
+                i++;
+        }
+
+        return 0;
 }
